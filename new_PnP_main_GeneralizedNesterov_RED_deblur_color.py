3b8af739ab51b997077ecd418d4a2c954610b940
Marien Renaud <Marien-RENAUD@users.noreply.github.com>
2025-04-04 11:38:07 +0200

    Add a parser argument for flexibily of the code.

--- new_PnP_main_GeneralizedNesterov_RED_deblur_color.py
+++ new_PnP_main_GeneralizedNesterov_RED_deblur_color.py
@@ -18,16 +18,23 @@ import utils_deblur as deblur
 sys.path.append("utils/models/") 
 from models.network_unet import UNetRes as Net
 from tqdm import tqdm
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('--model_path', type=str, default="models_ckpt/DRUNet_color.pth", help = "The path for the DRUNet pretrained weights")
+parser.add_argument('--n_channels', type=int, default=3, help = "number of channels of the image, by default RGB")
+parser.add_argument('--gpu_number', type=int, default=0, help = "the GPU number")
+parser.add_argument('--no_momentum', dest='no_momentum', action='store_true')
+parser.set_defaults(early_stopping=False)
+parser.add_argument('--r', type=int, default=3, help = "Parameter for the Generalized Nesterov momentum")
+parser.add_argument('--lamb', type=float, default=18, help = "Regularization parameter")
+parser.add_argument('--denoiser_level', type=float, default=0.1, help = "Denoiser level in [0.,1.]")
+parser.add_argument('--sigma_obs', type=float, default=12.75, help = "Standard variation of the noise in the observation in [0.,255.]")
+hparams = parser.parse_args()
 
-    
-model_path = "models_ckpt/DRUNet_color.pth"
+model_path = hparams.model_path
 n_channels = 3
-device = 'cuda:0'
-
-# model.eval()
-# model = model.cuda()
-
+device = torch.device('cuda:'+str(hparams.gpu_number) if torch.cuda.is_available() else 'cpu')
 
 class Drunet_running(torch.nn.Module):# DRUNet model definition 
     def __init__(self, model_path, n_channels, nc=[64, 128, 256, 512], nb=4, act_mode='R', downsample_mode="strideconv", upsample_mode="convtranspose", bias=False):
@@ -50,7 +57,6 @@ class Drunet_running(torch.nn.Module):# DRUNet model definition
         sigma_div_255 = torch.FloatTensor([sigma]).repeat(1, 1, x.shape[2], x.shape[3]).to(device)
         x = torch.cat((x, sigma_div_255), dim=1)
         return self.model(x)
-
 
 
 class PnP(nn.Module):
@@ -79,7 +85,7 @@ class PnP(nn.Module):
         pre_i = torch.clamp(u, 0., 1.)
         self.res['image'][i] = ToPILImage()(pre_i[0])
 
-    def forward(self, initial_uv, obs, clean, kernel, sigma_obs, lamb=690, denoiser_sigma=25./255., r=3, stepsize = 0.02):
+    def forward(self, initial_uv, obs, clean, kernel, sigma_obs, lamb=690, denoiser_sigma=25./255., r=3, momentum = True, stepsize = 0.02):
         '''
             Computed the Generalized Nesterov Algorithm with
                 initial_uv : the initialization for the algorithm
@@ -96,7 +102,6 @@ class PnP(nn.Module):
         u  = obs
         # average = obs
         K = kernel
-
         fft_k = deblur.p2o(K, u.shape[-2:])
         fft_kH = torch.conj(fft_k)
         abs_k = fft_kH * fft_k
@@ -120,10 +125,13 @@ class PnP(nn.Module):
             grad = reg_grad + lamb *data_grad
 
             x = y - stepsize*grad
-            if k+r==0:
-                y = x + (x-oldx)
+            if momentum:
+                if k+r==0:
+                    y = x + (x-oldx)
+                else:
+                    y = x + (k)/(k+r)*(x-oldx)
             else:
-                y = x + (k)/(k+r)*(x-oldx)
+                y = x
         return y_denoised
 
 def gen_data(clean_image, sigma, kernel, seed=0):
@@ -142,7 +150,7 @@ def gen_data(clean_image, sigma, kernel, seed=0):
     observation = observation_without_noise + noise
     return observation
 
-def plot_psnr(denoiser_level, lamb, sigma_obs, r):
+def plot_psnr(denoiser_level, lamb, sigma_obs, r, momentum):
     model = PnP()
     model.to(device)
     model.net.to(device)
@@ -164,9 +172,12 @@ def plot_psnr(denoiser_level, lamb, sigma_obs, r):
     kernel = kernel.to(device)
 
     with torch.no_grad():
-        model(initial_uv, observation, clean_image, kernel, sigma_obs, lamb, denoiser_level, r)
+        model(initial_uv, observation, clean_image, kernel, sigma_obs, lamb, denoiser_level, r, momentum)
 
-    savepth = 'images_GNesterov_RED_r{}'.format(r)+'/'
+    if momentum:
+        savepth = 'images_GNesterov_RED_r{}'.format(r)+'/'
+    else:
+        savepth = 'images_RED/'
     for j in range(len(model.res['image'])):
         # model.res['image'][j].save(savepth + 'result_Brain{}_{}.png'.format(i, j))
         model.res['image'][j].save(savepth + 'result_{}.png'.format(j))
@@ -177,10 +188,11 @@ def plot_psnr(denoiser_level, lamb, sigma_obs, r):
     plt.plot(x, y, '-', alpha=0.8, linewidth=1.5)
     plt.xlabel('iter')
     plt.ylabel('PSNR')
-    plt.savefig('PSNR_level{}_lamb{}_r{}_RED_GeneralizedNesterov.png'.format(denoiser_level, lamb,r))
+    if momentum:
+        plt.savefig('PSNR_level_{}_lamb{}_r{}_RED_GeneralizedNesterov.png'.format(denoiser_level, lamb,r))
+    else:
+        plt.savefig('PSNR_level_{}_lamb{}_RED.png'.format(denoiser_level, lamb,r))
 
 
-
-## RED 
-plot_psnr(denoiser_level = 0.1, lamb = 18, sigma_obs = 12.75, r = 4)
-
+# Run RED algorithm with or without Nesterov momentum
+plot_psnr(denoiser_level = hparams.denoiser_level, lamb = hparams.lamb, sigma_obs = hparams.sigma_obs, r = hparams.r, momentum = not(hparams.no_momentum))
