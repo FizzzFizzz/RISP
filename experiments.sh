5652b8ba138d4a3e0fb280c9cce7cdad71b35319
Marien Renaud <Marien-RENAUD@users.noreply.github.com>
2025-06-02 14:42:56 +0200

    New Push

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,4 @@
-for i in 0.05 0.01 0.005 0.001
+for i in 0.4 0.2
 do 
-    python RED_GNesterov.py --dataset_name "set1" --momentum --theta $i
+    python main.py --dataset_name "set5" --momentum --theta $i
 done
