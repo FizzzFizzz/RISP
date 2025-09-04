Correct MRI gradient and prox operator. Add speckle inverse problem.

algorithm.py
@@ -36,6 +36,9 @@ class PnP(nn.Module):
         elif denoiser_name == "GSDRUNet_SoftPlus":
             # The pretrained GSDRNet weights can be download in : https://plmbox.math.cnrs.fr/f/414fbb3e754840978ef8/?dl=1
             denoiser_net = deepinv.models.GSDRUNet(pretrained = "models_ckpt/GSDRUNet_SoftPlus.ckpt", act_mode = "s", device = device)
+        elif denoiser_name == "GSDRUNet_grayscale":
+            # The pretrained GSDRUnet weights in grayscale can be download in : https://huggingface.co/deepinv/gradientstep/resolve/main/GSDRUNet_grayscale_torch.ckpt
+            denoiser_net = deepinv.models.GSDRUNet(pretrained = "models_ckpt/GSDRUNet_grayscale.ckpt", in_channels=1, out_channels=1, device = device)
         else:
             raise ValueError("Denoiser not implemented.")
         self.Pb = Pb
@@ -107,7 +110,7 @@ class PnP(nn.Module):
 
         for k in tqdm(range(self.nb_itr)):
             x_old = x
-            self.get_psnr_i(torch.clamp(out, min = -0., max =1.), clean, k)
+            self.get_psnr_i(torch.clamp(torch.real(out), min = -0., max =1.), clean, k)
 
             if self.Pb == "inpainting" and k < 10:
                 sigma_den = 50. / 255.
@@ -120,6 +123,10 @@ class PnP(nn.Module):
             reg_grad = y - y_denoised
 
             if alg == "GD":
+                # print("reg")
+                # print(torch.sqrt(torch.sum(torch.abs(reg_grad)**2)))
+                # print("data")
+                # print(torch.sqrt(torch.sum(torch.abs(data_grad)**2)))
                 grad = reg_grad + lamb * data_grad
                 x = y - stepsize*grad
                 out = y_denoised
@@ -133,8 +140,6 @@ class PnP(nn.Module):
                 else: 
                     x = data_fidelity_prox_step(self, y - (stepsize/lamb)*reg_grad, obs, stepsize)
                     out = y_denoised
-                
-
 
             if restarting_su:
                 restart_crit_su_old = restart_crit_su
@@ -169,4 +174,4 @@ class PnP(nn.Module):
                 y = x + (1-theta)*(x - x_old)
             else:
                 y = x
-        self.get_psnr_i(torch.clamp(out, min = -0., max =1.), clean, self.nb_itr) # put the last iterate at the end of the stack
+        self.get_psnr_i(torch.clamp(torch.real(out), min = -0., max =1.), clean, self.nb_itr) # put the last iterate at the end of the stack

forward_model.py
@@ -18,7 +18,7 @@ import utils_sr
 from argparse import ArgumentParser
 from denoiser_model import *
 
-def gen_data(self, clean_image, sigma, seed=0):
+def gen_data(self, clean_image, sigma = None, seed=0):
     """
     Generate the degradate observation
     """
@@ -40,11 +40,15 @@ def gen_data(self, clean_image, sigma, seed=0):
         observation = observation_without_noise + noise
         return observation, mask
     elif self.Pb == "MRI":
-        M = genMask(init.shape[-2:], self.numLines, device=self.device)
-        observation_without_noise = M[None,None,:,:] * torch.fft.fft2(clean_image)
+        M = genMask(clean_image.shape[-2:], self.numLines, device=self.device)
+        observation_without_noise = M[None,None,:,:] * fft2c(clean_image)
         noise = (torch.normal(torch.zeros(observation_without_noise.size()), torch.ones(observation_without_noise.size()), generator = gen)*sigma / 255).to(self.device)
         observation = observation_without_noise + noise
-        return observation, M
+        pseudo_inverse = torch.real(ifft2c(mask*observation.clone()))
+        return observation, M, pseudo_inverse
+    elif self.Pb == "speckle":
+        observation = injectspeckle_amplitude_log(clean_image, L = self.L, device = self.device, gen = gen)
+        return observation
     else:
         raise ValueError("Forward Model not implemented")
 
@@ -67,15 +71,16 @@ def data_fidelity_init(self, init = None):
     elif self.Pb == 'inpainting':
         self.M = (self.mask).clone()
     elif self.Pb == "MRI":
-        self.neg_M = torch.ones(self.M.shape).to(device) - self.M
+        self.neg_M = torch.ones(self.M.shape).to(self.device) - 1*self.M
     elif self.Pb == 'ODT':
-        # print('ODT loves you~')
         from utils.inverse_scatter import InverseScatter
         self.scatter_op = InverseScatter(
             Lx=0.18, Ly=0.18, Nx=128, Ny=128, 
             wave=6, numRec=180, numTrans=20,
             sensorRadius=1.6, svd=False
         )
+    elif self.Pb == "speckle":
+        self.f_speckle = lambda u, y : torch.sum(self.L * (u + torch.exp(y - u)))
     else:
         raise ValueError("Forward Model not implemented")
 
@@ -88,7 +93,7 @@ def compute_data_grad(self, x, obs):
         elif self.Pb == 'inpainting':
             data_grad = 2 * self.M * (x - obs)
         elif self.Pb == 'MRI':
-            data_grad = torch.fft.ifft2(self.M * (torch.fft.fft2(x) - y))
+            data_grad = torch.real(ifft2c(self.M * (fft2c(x) - obs)))
         elif self.Pb == 'ODT':
             with torch.enable_grad():
                 v = x.clone() 
@@ -102,6 +107,8 @@ def compute_data_grad(self, x, obs):
                 data_grad = norm_grad
         else:
             raise ValueError("Forward Model not implemented")
+    elif self.noise_model == 'speckle':
+        return self.L*(1-torch.exp(obs-x))
     else :  
         ValueError('Forward Model noise model not implemented')
     return data_grad
@@ -118,8 +125,8 @@ def data_fidelity_prox_step(self, x, y, stepsize):
                 px = (stepsize*self.M*y + x)/(stepsize*self.M+1)
             else :
                 px = self.M*y + (1-self.M)*x
-        elif self.Pb = 'MRI':
-            px = torch.fft.ifft2(((1/(1+self.stepsize))*self.M+self.neg_M)*(torch.fft.fft2(x)+self.stepsize*self.M*y))
+        elif self.Pb == 'MRI':
+            px = ifft2c(((1/(1+self.stepsize))*self.M+self.neg_M)*(fft2c(x)+self.stepsize*self.M*y))
         elif self.Pb == 'ODT':
             input = x
             uu = input.clone()
@@ -136,11 +143,25 @@ def data_fidelity_prox_step(self, x, y, stepsize):
                     vv -= data_grad*0.1
                     vv = torch.clamp(vv,-0,1)
             x = uu
-            # by wdl
             x = torch.clamp(x,-0,1)
             px = x
         else:
             ValueError('Forward Model degradation not implemented')
+    elif self.noise_model == 'speckle': 
+        # Implement a gradient descent to approximate the Prox
+        input = x
+        vv = input.clone()
+        with torch.enable_grad():
+            for _ in range(5):
+                uu = vv.clone()
+                uu = uu.detach().requires_grad_(True)
+                norm = (0.5/stepsize)*torch.sum(torch.abs(uu - y)**2) + self.f_speckle(uu, y)
+                norm_grad = torch.autograd.grad(outputs=norm, inputs=uu)[0]
+                vv -= norm_grad*0.001
+                vv = torch.clamp(vv,-0,1)
+        px = vv
+        print(px)
+        px = torch.clamp(px,-0,1)
     else :  
         ValueError('Forward Model noise model not implemented')
     return px
@@ -192,3 +213,43 @@ def genMask(imgSize, numLines, device='cpu'):
         mask[iy, ix] = True
     
     return mask
+
+def fft2c(x):
+    """
+    2D centered FFT (unitary)
+    """
+    return torch.fft.fftshift(
+        torch.fft.fft2(
+            torch.fft.ifftshift(x, dim=(-2, -1)),
+            norm="ortho"
+        ),
+        dim=(-2, -1)
+    )
+
+def ifft2c(k):
+    """
+    2D centered iFFT (unitary)
+    """
+    return torch.fft.fftshift(
+        torch.fft.ifft2(
+            torch.fft.ifftshift(k, dim=(-2, -1)),
+            norm="ortho"
+        ),
+        dim=(-2, -1)
+    )
+
+def injectspeckle_amplitude_log(img, L, device, gen):
+    """
+    Injected speckle noise in the image img with the number of looks L
+    """
+    img_exp = torch.exp(img)
+    rows = img_exp.shape[-2]
+    columns = img_exp.shape[-1]
+    s = torch.zeros((rows, columns))
+    for k in range(0,L):
+        gamma = torch.abs( torch.randn(rows,columns, generator = gen) + torch.randn(rows,columns, generator = gen)*1j )**2/2
+        s = s + gamma
+    s_amplitude = torch.sqrt(s/L).to(device)
+    ima_speckle_amplitude = img_exp * s_amplitude[None,None,:,:]
+    ima_speckle_log = torch.log(ima_speckle_amplitude)
+    return ima_speckle_log

generate_results.py
@@ -157,6 +157,8 @@ if pars.fig_number == 2:
     method_name = dic['method_name']
     residuals = dic['residuals']
 
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    color_indx = [3, 1, 2, 0]
     fig = plt.figure()
 
     for j in range(nb_method):
@@ -165,18 +167,18 @@ if pars.fig_number == 2:
         psnr_std = np.std(psnr_list[j], axis = 0)
         psnr_min = np.quantile(psnr_list[j], 0.25, axis = 0)
         psnr_max = np.quantile(psnr_list[j], 0.75, axis = 0)
-        line, = plt.plot(np.arange(len(psnr_mean[:convergence_time+5])), psnr_mean[:convergence_time+5], label = method_name[j])
+        line, = plt.plot(np.arange(len(psnr_mean)), psnr_mean, label = method_name[j], color = colors[color_indx[j]])
         # plt.fill_between(np.arange(len(psnr_mean)), psnr_min, psnr_max, alpha=0.1, color=line.get_color())
 
     size_number = 15
 
-    plt.plot([0, 50], [27.7, 27.7], linestyle = "--", color = 'gray')
+    # plt.plot([0, 50], [27.7, 27.7], linestyle = "--", color = 'gray')
 
     plt.xlim(0,49)
     plt.xticks([0, 9, 20, 38, 49], ["0", "9", "20", "38", "50"], fontsize = size_number)
-    plt.ylim(18,28)
-    plt.yticks([18, 27.7], ["18", "27.7"], fontsize = size_number)
-    plt.legend()
+    plt.ylim(18,29)
+    plt.yticks([18, 29], ["18", "29"], fontsize = size_number)
+    # plt.legend()
     plt.title(r"Convergence PSNR for deblurring")
     fig.savefig(path_figure+'convergence_PSNR_deblurring.png', dpi = 300)
     plt.show()
@@ -188,7 +190,7 @@ if pars.fig_number == 2:
         residuals_mean = np.mean(np.log10(residuals[j]), axis = 0)
         residuals_min = np.quantile(np.log10(residuals[j]), 0.25, axis = 0)
         residuals_max = np.quantile(np.log10(residuals[j]), 0.75, axis = 0)
-        line, = plt.plot(np.arange(len(residuals_mean)), residuals_mean, label = method_name[j])
+        line, = plt.plot(np.arange(len(residuals_mean)), residuals_mean, label = method_name[j], color = colors[color_indx[j]])
         plt.fill_between(np.arange(len(residuals_mean)), residuals_min, residuals_max, alpha=0.1, color=line.get_color())
 
     plt.xlim(0,49)
@@ -201,7 +203,7 @@ if pars.fig_number == 2:
     plt.yticks(ticks, labels, fontsize = size_number)
     plt.ylabel(r"$\|x^{k+1} - x^k\|^2 / \|x^0\|^2$", labelpad=-10, fontsize = size_number)
 
-    plt.legend()
+    # plt.legend()
     plt.title(r"Convergence residuals for deblurring")
     fig.savefig(path_figure+'convergence_residuals_deblurring.png', dpi = 300)
     plt.show()
@@ -288,7 +290,7 @@ if pars.fig_number == 4:
     # Generate a figure to show various result on image deblurring with various kernels
     path_result = "results/deblurring/CBSD10/"
 
-    n = 5
+    n = 7
     m = 6
 
     size_title = 40
@@ -312,7 +314,7 @@ if pars.fig_number == 4:
         ProxRED = (np.array(dic_ProxRED["restored"]).astype(np.uint8), dic_ProxRED["psnr_restored"], dic_ProxRED["ssim_restored"])
         ProxRiRED = (np.array(dic_ProxRiRED["restored"]).astype(np.uint8), dic_ProxRiRED["psnr_restored"], dic_ProxRiRED["ssim_restored"])
         GT = np.array(dic_RED["clean_image"]).astype(np.uint8)
-        k = dic_RED["kernel"]
+        kern = dic_RED["kernel"]
         obs = np.array(dic_RED["observation"]); gt = np.array(dic_RED["clean_image"])
         Obs = (obs.astype(np.uint8), PSNR(obs, gt), ssim(obs, gt, channel_axis = -1))
 
@@ -376,15 +378,18 @@ if pars.fig_number == 4:
 
         for j, im in enumerate(im_list):
             ax = plt.subplot(gs[i, 1+j])
+            # Add the kernel on the image
             if j==0:
                 c = 100
                 if i == 6:
                     big_kernel = np.zeros((17,17))
-                    big_kernel[4:13, 4:13] = k
+                    big_kernel[4:13, 4:13] = kern
                     k_resize = cv2.resize(big_kernel, dsize =(c,c), interpolation=cv2.INTER_NEAREST)
                 else:
-                    k_resize = cv2.resize(k, dsize =(c,c), interpolation=cv2.INTER_NEAREST)
-                im[0][-k_resize.shape[0]:,:k_resize.shape[1]] = k_resize[:,:,None]*np.ones(3)[None,None,:] / np.max(k_resize)
+                    k_resize = cv2.resize(kern, dsize =(c,c), interpolation=cv2.INTER_NEAREST)
+                k_resize = (k_resize - np.min(k_resize)) / (np.max(k_resize) - np.min(k_resize))
+                k_resize = (255 * k_resize).astype(np.uint8)
+                im[0][-k_resize.shape[0]:,:k_resize.shape[1]] = k_resize[:,:,None]*np.ones(3)[None,None,:]
             
             #add a zoom of the image
             patch_c = cv2.resize(im[0][y_c:y_c+hei, x_c:x_c+wid], dsize =(c_patch,c_patch), interpolation=cv2.INTER_CUBIC)
@@ -586,7 +591,27 @@ if pars.fig_number == 6:
     plt.show()
 
 
-
+if pars.fig_number == 7:
+    path_result = "results/deblurring/CBSD10/"
+    method_name = ["RED", r"RiRED $\theta = 0.2$", "Prox-RED", r"Prox-RiRED $\theta = 0.2$"]
+    save_name = ["RED", "RiRED", "Prox_RED", "Prox_RiRED"]
+    stepsize_method = [0.1, 0.07, 2.0, 5.0]
+    nb_method = len(method_name)
+    psnr_list = [[] for _ in range(nb_method)]
+    residuals = [[] for _ in range(nb_method)]
+    i = 9
+    k = 0
+    path_method = ["GD/denoiser_name_GSDRUNet_SoftPlus/kernel_"+str(k)+"/", "GD/denoiser_name_GSDRUNet_SoftPlus/kernel_"+str(k)+"/Momentum/theta_0.2/restarting_li/", "PGD/denoiser_name_GSDRUNet_SoftPlus/kernel_"+str(k)+"/", "PGD/denoiser_name_GSDRUNet_SoftPlus/kernel_"+str(k)+"/Momentum/theta_0.2/restarting_li/"]
+    for j in range(nb_method):
+        stepsize = str(stepsize_method[j])
+        dic = np.load(path_result + path_method[j] + "/den_level_0.1/lamb_15.0/nb_itr_9/stepsize_"+stepsize+"/dict_results_"+str(i)+".npy", allow_pickle=True).item()
+        
+        im = dic['restored']
+        # print(im.shape())
+        # print(np.max(im))
+        # print(np.min(im))
+        # plt.imsave(path_figure+'restored_9_itr_'+save_name[j]+'.png', im)
+        im.save(path_figure+'restored_9_itr_'+save_name[j]+'_psnr_'+str(dic['psnr_restored'])+'.png')
 
 
 # if pars.fig_number == 2:
@@ -912,6 +937,22 @@ if pars.table_number == 2:
     output_ssim = np.array(output_ssim)
     for j in range(nb_method):
         print(method_name[j], " : PSNR/SSIM : {:.2f} & {:.2f}".format(np.mean(output_psnr[j]),np.mean(output_ssim[j])))
+
+
+
+if pars.table_number == 3:
+    #generate the result of the grid-search for speckle L = 10
+    path_result = "results/speckle/setSAR4/GD/denoiser_name_GSDRUNet_grayscale/L_10/"
+    lamb_list = ["80.0", "100.0", "150.0", "200.0"]
+    sigma_list = ["0.15", "0.2", "0.25", "0.3"]
+    n = 4
+    for std in sigma_list:
+        for lamb in lamb_list:
+            output_psnr = []
+            for j in range(n):
+                dic_RED = np.load(path_result + "den_level_" + std +"/lamb_" + lamb +"/stepsize_0.001/dict_results_"+str(j)+".npy", allow_pickle=True).item()
+                output_psnr.append(dic_RED["psnr_restored"])
+            print("Sigma ",std,"Lambda ",lamb, ":", np.mean(output_psnr))
 
 
 # if pars.fig_number == 4:

main.py
@@ -17,12 +17,12 @@ import utils_deblur as deblur
 from argparse import ArgumentParser
 from algorithm import *
 from forward_model import *
+from time import time
 
 parser = ArgumentParser()
 parser.add_argument('--model_path', type=str, default="models_ckpt/drunet_color.pth", help = "The path for the DRUNet pretrained weights")
 parser.add_argument('--denoiser_name', type=str, default="GSDRUNet", help = "Type of denoiser, DRUNet, GSDRUNet or GSDRUNet_SoftPlus are implemented")
 parser.add_argument('--Pb', type=str, default="deblurring", help = "Inverse problem to tackle: deblurring, inpainting, ODT")
-parser.add_argument('--n_channels', type=int, default=3, help = "number of channels of the image, by default RGB")
 parser.add_argument('--gpu_number', type=int, default=0, help = "the GPU number")
 parser.add_argument('--Nesterov', dest='Nesterov', action='store_true')
 parser.set_defaults(Nesterov=False)
@@ -48,6 +48,7 @@ parser.add_argument('--nb_itr', type=int, default=50, help = "Number of iteratio
 parser.add_argument('--theta', type=float, default=0.9, help = "Momentum parameter")
 parser.add_argument('--start_im_indx', type=int, default=0, help = "Rank of the image to start from in the dataset")
 parser.add_argument('--p', type=float, default=0.5, help = "Proportion of viewed pixels for inpainting with random mask")
+parser.add_argument('--L', type=int, default=10, help = "Number of looks for image depseckling")
 parser.add_argument('--reduction_factor', type=int, default=8, help = "Factor of acceleration for MRI")
 parser.add_argument('--dont_save_images', dest='dont_save_images', action='store_true')
 parser.set_defaults(dont_save_images=False)
@@ -59,9 +60,8 @@ Pb = hparams.Pb
 denoiser_name = hparams.denoiser_name
 model_path = hparams.model_path
 nb_itr = hparams.nb_itr
-n_channels = hparams.n_channels
+# n_channels = hparams.n_channels
 device = torch.device('cuda:'+str(hparams.gpu_number) if torch.cuda.is_available() else 'cpu')
-
 
 # Parameters setting
 denoiser_level = hparams.denoiser_level
@@ -73,14 +73,16 @@ theta = hparams.theta
 Nesterov = hparams.Nesterov
 momentum = hparams.momentum
 grayscale = hparams.grayscale
+if Pb == 'MRI' and not('--grayscale' in sys.argv):
+    grayscale = True
 restarting_su = hparams.restarting_su
 restarting_li = hparams.restarting_li
 stepsize = hparams.stepsize
 dont_save_images = hparams.dont_save_images
 save_each_itr = hparams.save_each_itr
 alg = hparams.alg
-
 Average_PSNR = 0
+L = hparams.L
 
 # Set input image paths
 input_path = os.path.join('datasets', hparams.dataset_name)
@@ -95,8 +97,12 @@ for i in range(hparams.start_im_indx, len(input_paths)):
     model.net.to(device)
 
     # Load the clean image
-    clean_image = util.imread_uint(clean_image_path, 3)
-    clean_image = util.single2tensor3(clean_image).unsqueeze(0) /255.
+    if not(grayscale):
+        clean_image = util.imread_uint(clean_image_path, 3)
+        clean_image = util.single2tensor3(clean_image).unsqueeze(0) /255.
+    else:
+        clean_image = util.imread_uint(clean_image_path, 1)
+        clean_image = util.single2tensor3(clean_image).unsqueeze(0) /255.
 
     if Pb == "deblurring":
         if '--kernel_index' in sys.argv:
@@ -132,12 +138,25 @@ for i in range(hparams.start_im_indx, len(input_paths)):
     elif Pb == "MRI":
         clean_image = clean_image.to(device)
         model.numLines = int(clean_image.shape[-1] / hparams.reduction_factor)
-        observation, mask = gen_data(model, clean_image, sigma_obs)
+        observation, mask, pseudo_inverse = gen_data(model, clean_image, sigma_obs)
         model.M = mask
+        initial_uv = pseudo_inverse
+
+    elif Pb == "speckle":
+        clean_image = clean_image.to(device)
+        model.noise_model = "speckle"
+        model.L = L
+        observation = gen_data(model, clean_image)
+        initial_uv = observation
+
+    time_restore = time()
 
     # Run RED algorithm with or without momentum
     with torch.no_grad():
         model.forward(initial_uv, observation, clean_image, sigma_obs, lamb, denoiser_level, theta, r, B, Nesterov, momentum, restarting_su, restarting_li, stepsize, alg)
+
+    time_restore = time() - time_restore
+    print("The time of restoration is : ", time_restore)
 
     psnr_list = model.res['psnr']
     ssim_list = model.res['ssim']
@@ -161,6 +180,9 @@ for i in range(hparams.start_im_indx, len(input_paths)):
         os.makedirs(savepth, exist_ok = True)
     if '--kernel_index' in sys.argv:
         savepth = os.path.join(savepth, 'kernel_'+str(k_index))
+        os.makedirs(savepth, exist_ok = True)
+    if '--L' in sys.argv:
+        savepth = os.path.join(savepth, 'L_'+str(L))
         os.makedirs(savepth, exist_ok = True)
     if '--reduction_factor' in sys.argv:
         savepth = os.path.join(savepth, 'reduction_factor_'+str(hparams.reduction_factor))
@@ -196,6 +218,10 @@ for i in range(hparams.start_im_indx, len(input_paths)):
     print("Results are saved in the path : ", savepth)
 
     if not(dont_save_images):
+        if grayscale :
+            colormap = 'gray'
+        else:
+            colormap = 'viridis'
         if save_each_itr:
             savepth_img = savepth+"/set_img_{}/".format(i)
             os.makedirs(savepth_img, exist_ok = True)
@@ -205,8 +231,8 @@ for i in range(hparams.start_im_indx, len(input_paths)):
         model.res['image'][-1].save(savepth + "/{}_restored_img.png".format(i))
         clean_img_uint = util.tensor2uint(clean_image)
         obs_uint = util.tensor2uint(observation)
-        plt.imsave(savepth + "/{}_clean_img.png".format(i), clean_img_uint)
-        plt.imsave(savepth + "/{}_observation.png".format(i), obs_uint)
+        plt.imsave(savepth + "/{}_clean_img.png".format(i), clean_img_uint, cmap = colormap)
+        plt.imsave(savepth + "/{}_observation.png".format(i), obs_uint, cmap = colormap)
 
         itr_list = range(len(psnr_list))
         plt.clf()
@@ -238,6 +264,7 @@ for i in range(hparams.start_im_indx, len(input_paths)):
             'ssim_restored' : ssim_list[-1],
             'restored' : model.res['image'][-1],
             'nb_restart_activ' : model.nb_restart_activ,
+            'grayscale' : grayscale,
         }
     
     if Pb == "deblurring":